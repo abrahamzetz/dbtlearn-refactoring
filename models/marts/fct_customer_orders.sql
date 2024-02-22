eca2db584aa8a7050e4f996fe6e343462f69a07e
Abraham Setiawan <abraham.setiawan@solita.fi>
2024-02-22 16:15:18 +0000

    cte grouping

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,29 +1,68 @@
-WITH paid_orders as (select Orders.ID as order_id,
+WITH 
+
+-- Import CTEs
+
+customers as (
+    select * from {{ source('jaffle_shop', 'customers') }}
+),
+
+orders as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+
+payments as (
+    select * from {{ source('stripe', 'payment') }}
+),
+
+total_amount_paid as (
+    select 
+        ORDERID as order_id, 
+        max(CREATED) as payment_finalized_date, 
+        sum(AMOUNT) / 100.0 as total_amount_paid
+    from payments
+    where STATUS <> 'fail'
+    group by 1
+),
+
+clv as (
+    select
+        p.order_id,
+        sum(t2.total_amount_paid) as clv_bad
+    from paid_orders p
+    left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
+    group by 1
+    order by p.order_id
+),
+
+
+paid_orders as (
+    select Orders.ID as order_id,
         Orders.USER_ID    as customer_id,
         Orders.ORDER_DATE AS order_placed_at,
-            Orders.STATUS AS order_status,
+        Orders.STATUS AS order_status,
         p.total_amount_paid,
         p.payment_finalized_date,
         C.FIRST_NAME    as customer_first_name,
             C.LAST_NAME as customer_last_name
-    FROM {{ source('jaffle_shop', 'orders') }} as Orders
-    left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-from {{ source('stripe', 'payment') }}
-where STATUS <> 'fail'
-group by 1) p ON orders.ID = p.order_id
-left join {{ source('jaffle_shop', 'customers') }} C on orders.USER_ID = C.ID ),
+    FROM orders
+    left join total_amount_paid p 
+        ON orders.ID = p.order_id
+    left join customers C on orders.USER_ID = C.ID 
+),
 
-customer_orders 
-    as (select C.ID as customer_id
+customer_orders as (
+    select C.ID as customer_id
         , min(ORDER_DATE) as first_order_date
         , max(ORDER_DATE) as most_recent_order_date
         , count(ORDERS.ID) AS number_of_orders
-    from {{ source('jaffle_shop', 'customers') }} C 
-    left join {{ source('jaffle_shop', 'orders') }} as Orders
+    from customers C 
+    left join orders
     on orders.USER_ID = C.ID 
-    group by 1)
+    group by 1
+),
 
-select
+final as (
+    select
     p.*,
     ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
     ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
@@ -34,14 +73,9 @@ select
     c.first_order_date as fdos
     FROM paid_orders p
     left join customer_orders as c USING (customer_id)
-    LEFT OUTER JOIN 
-    (
-            select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x on x.order_id = p.order_id
+    LEFT OUTER JOIN clv_bad x 
+        on x.order_id = p.order_id
     ORDER BY order_id
+)
+
+select * from final
