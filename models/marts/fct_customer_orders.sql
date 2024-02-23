ab11dd43b014d110a92435a698bc1bf9067b4aa9
Abraham Setiawan <abraham.setiawan@solita.fi>
2024-02-23 08:22:22 +0000

    clean code

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,6 +1,6 @@
-WITH 
+with
 
--- Import CTEs
+-- import ctes
 
 customers as (
     select * from {{ source('jaffle_shop', 'customers') }}
@@ -15,12 +15,12 @@ payments as (
 ),
 
 total_amount_paid as (
-    select 
-        ORDERID as order_id, 
-        max(CREATED) as payment_finalized_date, 
-        sum(AMOUNT) / 100.0 as total_amount_paid
+    select
+        orderid as order_id,
+        max(created) as payment_finalized_date,
+        sum(amount) / 100.0 as total_amount_paid
     from payments
-    where STATUS <> 'fail'
+    where status <> 'fail'
     group by 1
 ),
 
@@ -36,46 +36,46 @@ clv as (
 
 
 paid_orders as (
-    select Orders.ID as order_id,
-        Orders.USER_ID    as customer_id,
-        Orders.ORDER_DATE AS order_placed_at,
-        Orders.STATUS AS order_status,
+    select orders.id as order_id,
+        orders.user_id    as customer_id,
+        orders.order_date as order_placed_at,
+        orders.status as order_status,
         p.total_amount_paid,
         p.payment_finalized_date,
-        C.FIRST_NAME    as customer_first_name,
-            C.LAST_NAME as customer_last_name
-    FROM orders
-    left join total_amount_paid p 
-        ON orders.ID = p.order_id
-    left join customers C on orders.USER_ID = C.ID 
+        c.first_name    as customer_first_name,
+            c.last_name as customer_last_name
+    from orders
+    left join total_amount_paid p
+        on orders.id = p.order_id
+    left join customers c on orders.user_id = c.id
 ),
 
 customer_orders as (
-    select C.ID as customer_id
-        , min(ORDER_DATE) as first_order_date
-        , max(ORDER_DATE) as most_recent_order_date
-        , count(ORDERS.ID) AS number_of_orders
-    from customers C 
+    select customers.id as customer_id
+        , min(orders.order_date) as first_order_date
+        , max(orders.order_date) as most_recent_order_date
+        , count(orders.id) as number_of_orders
+    from customers
     left join orders
-    on orders.USER_ID = C.ID 
+    on orders.user_id = customers.id
     group by 1
 ),
 
 final as (
     select
     p.*,
-    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-    CASE WHEN c.first_order_date = p.order_placed_at
-    THEN 'new'
-    ELSE 'return' END as nvsr,
+    row_number() over (order by p.order_id) as transaction_seq,
+    row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
+    case when c.first_order_date = p.order_placed_at
+    then 'new'
+    else 'return' end as nvsr,
     x.clv_bad as customer_lifetime_value,
     c.first_order_date as fdos
-    FROM paid_orders p
-    left join customer_orders as c USING (customer_id)
-    LEFT OUTER JOIN clv_bad x 
+    from paid_orders p
+    left join customer_orders as c using (customer_id)
+    left outer join clv_bad x
         on x.order_id = p.order_id
-    ORDER BY order_id
+    order by order_id
 )
 
 select * from final
